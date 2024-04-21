2c86debf12b90753e014a46f8c1ffacdf66ef051
shannydi <71073299+shannydi@users.noreply.github.com>
2024-04-21 17:53:23 +0300

    Add files via upload

---
+++ app_run.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Create a simple Streamlit app
+st.title('Hello, Streamlit!')
+
+st.write("""
+This is a simple Streamlit app. You can add more widgets and functions to make it interactive.
+""")
+
+st.button('Click me!')
